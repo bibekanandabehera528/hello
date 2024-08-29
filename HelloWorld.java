14f4913a2f55f93f9ad89e667c7fcc6b097671ef
bibekanandabehera528 <bibekanandabehera528@gmail.com>
2024-08-30 00:48:40 +0530

    vivek commit first

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,5 @@
 class HelloWorld{
 	public static void main(String [] args){
-<<<<<<< HEAD
-		System.out.println("Hello World Vivek");
-=======
-		System.out.println("Hello World Vivek Santi");
->>>>>>> vivek
+		System.out.println("Hello Vivek and Santi");
 	}
 }
